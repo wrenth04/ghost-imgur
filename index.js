ec96a72340f1e310a7acc9aa456805e841bc9947
Wei <wrenth04@gmail.com>
2016-02-24 14:43:33 +0800

    add save function

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var imgur = require('imgur');
+var Promise = require('bluebird');
+
+function ghostImgur(config) {
+  this.config = config || {};
+}
+
+ghostImgur.prototype.save = function(file) {
+  return new Promise(function(resolve, reject) {
+    imgur.uploadFile(file.path)
+      .then(function(json) {
+        if(!json || !json.data || !json.data.link) return reject();
+        resolve(json.data.link);
+      })
+      .catch(reject);
+  });
+}
+
+ghostImgur.prototype.serve = function() {
+  return function(req, res, next) {
+  };
+}
+
+module.exports = ghostImgur;
