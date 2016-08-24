0356bd5a1338acf385ef7e9a203fb14fc75921e2
wei <wrenth04@gmail.com>
2016-08-24 21:33:22 +0800

    migrate to Ghost 0.10.0 #3

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict'
 
+var util = require('util');
+var BaseStorage = require('../../../core/server/storage/base');
 var imgur = require('imgur');
 var Promise = require('bluebird');
 
 function ghostImgur(config) {
   this.config = config || {};
+  BaseStorage.call(this);
 }
+
+util.inherits(ghostImgur, BaseStorage);
 
 ghostImgur.prototype.save = function(file) {
   return new Promise(function(resolve, reject) {
